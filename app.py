d5f4471c291b6b6602c66e483503d1449ef609e7
shkier26-spec <shkier26@gmail.com>
2026-03-23 11:06:53 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+import requests
+import os
+
+app = Flask(__name__)
+
+TOKEN = "8603724750:AAF6upsTbCiuqH7wbAaWo0F75WzhOlqDsTs"
+CHAT_ID = "958840368"
+
+@app.route('/')
+def home():
+    return "Bot is running"
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.json
+    msg = data.get("message", "🔥 Signal received")
+
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+    requests.post(url, data={
+        "chat_id": CHAT_ID,
+        "text": msg
+    })
+
+    return "ok"
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
